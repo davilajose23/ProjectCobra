f6e6794884cc68ead7616c39034c87618ecbaf7d
Jose Fernando Davila Orta <davila.jose23@gmail.com>
2017-03-05 13:38:24 -0600

    added all Regular Exp

--- scanner.py
+++ scanner.py
@@ -6,33 +6,33 @@ import ply.lex as lex
 # List of reserved words in the leanguage
 
 reserved = {
-	'if': 'IF',
-	'end': 'END',
-	'while': 'WHILE',
-	'print': 'PRINT',
-	'read': 'READ',
-	'else': 'ELSE',
-	'and': 'AND',
-	'or': 'OR',
-	'not': 'NOT',
-	'for': 'FOR',
-	'return': 'RETURN',
-	'start': 'START',
-	'true': 'TRUE',
-	'false': 'FALSE',
-	'bool': 'BOOL',
-	'int': 'INT',
-	'double': 'DOUBLE'
-	'string': 'STRING',
-	'void': 'VOID',
-	'break': 'BREAK',
-	'from': 'FROM',
-	'to': 'TO',
-	'mod': 'MOD',
-	'use': 'USE',
-	'lang': 'LANG',
-	'es': 'SPANISH',
-	'en': 'ENGLISH'
+    'if': 'IF',
+    'end': 'END',
+    'while': 'WHILE',
+    'print': 'PRINT',
+    'read': 'READ',
+    'else': 'ELSE',
+    'and': 'AND',
+    'or': 'OR',
+    'not': 'NOT',
+    'for': 'FOR',
+    'return': 'RETURN',
+    'true': 'TRUE',
+    'false': 'FALSE',
+    'bool': 'BOOL',
+    'int': 'INT',
+    'double': 'DOUBLE'
+    'string': 'STRING',
+    'void': 'VOID',
+    'break': 'BREAK',
+    'from': 'FROM',
+    'to': 'TO',
+    'mod': 'MOD',
+    'use': 'USE',
+    'lang': 'LANG',
+    'es': 'SPANISH',
+    'en': 'ENGLISH',
+    'func': 'FUNCTION'
 }
 
 # List of token names.   This is always required
@@ -53,10 +53,10 @@ tokens = [
    'TIMES_EQUALS',
    'DIVIDE_EQUALS',
    'PERCENTAGE',
-   'LPARENTHESIS',
-   'RPARENTHESIS',
-   'LBRACKET',
-   'RBRACKET',
+   'LEFT_PARENTHESIS',
+   'RIGHT_PARENTHESIS',
+   'LEFT_BRACKET',
+   'RIGHT_BRACKET',
    'COMMENT',
    'EOL',
    'SPACE',
@@ -64,15 +64,81 @@ tokens = [
    'COLON'
 ] + list(reserved.values())
 
+#Regular expression rules for simple tokens
+t_PLUS                  = r'\+'
+t_MINUS                 = r'-'
+t_TIMES                 = r'\*'
+t_DIVIDES               = r'/'
+t_LESS                  = r'<'
+t_GREATER               = r'>'
+t_EQUALS                = r'='
+t_EQUALS_EQUALS         = r'=='
+t_GRATER_EQUALS         = r'>='
+t_LESS_EQUALS           = r'<='
+t_COMMA                 = r'\,'
+t_COLON                 = r':'
+t_NOT_EQUALS            = r'!='
+t_PLUS_EQUALS           = r'+='
+t_MINUS_EQUALS          = r'-='
+t_TIMES_EQUALS          = r'*='
+t_DIVIDE_EQUALS         = r'/='
+t_PERCENTAGE            = r'%'
+t_LEFT_PARENTHESIS      = r'\('
+t_RIGHT_PARENTHESIS     = r'\)'
+t_LEFT_BRACKET          = r'\['
+t_RIGHT_BRACKET         = r'\]'
 
-# Build the parser
-parser = yacc.yacc()
 
-while True:
-   try:
-       s = raw_input('> ')
-   except EOFError:
-       break
-   if not s: continue
-   result = parser.parse(s)
-   print(result)
+def t_IDENTIFIER(t):
+    r'[a-zA-Z](_?[a-zA-Z0-9]+)*'
+    t.type = reserved.get(t.value,'IDENTIFIER') # Check for reserved words
+    return t
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+def t_COMMENT(t):
+    r'\#.*'
+    pass
+    
+def t_DOUBLE (t):
+    r'([-]?[0-9]+[.])[0-9]+'
+    t.value = float(t.value)
+    return t
+
+def t_INT(t):
+    r'[-]?[0-9]+'
+    t.value = int(t.value)
+    return t
+
+def t_STRING(t):
+    r'\'(\'\'|[^\n\t])*\''
+    t.value = str(t)
+    return t
+
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
+
+# Test it out
+data = '''program p1; var fac1, fac2:int;
+a = 1
+func hola() a = 2
+print a
+'''
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+for tok in lexer:
+    print(tok)
