a5109d659473f5cdbc679ce4a2bc6d1c87e4e13b
Jose Fernando Davila Orta <davila.jose23@gmail.com>
2017-04-07 18:18:48 -0500

    virtualmachine mods

--- VirtualMachine.py
+++ VirtualMachine.py
@@ -1,30 +1,32 @@
 from quad_generator import Variable, Quadruple, QuadGenerator
 
 
-class VirtualMachine(self):
-     def __init__(self):
-        quadruples = []
-        pc = 0
 
-    def readFile():
+class VirtualMachine():
+    def __init__(self):
+        self.quadruples = []
+        self.pc = 0
+
+    def readFiles(self):
         cont = 0
         f = open('output.cob', 'r')
         for line in f:
-            print(line),
+            # print(line),
             q = line[1:-2].split(',')
             quad = Quadruple(cont, q[0], q[1], q[2], q[3])
             cont += 1
-            quadruples.append(quad)
+            self.quadruples.append(quad)
         f.close()
     
-    def run():
-        readFile()
+    def run(self):
+        self.readFiles()
 
-        while quadruples[pc].op != 'END':
-            quad = quadruples[pc]
+        while self.quadruples[self.pc].op != 'END':
+            quad = self.quadruples[self.pc]
+            
             op = quad.op
-            if op == 'print':
-                print(quadruples[pc].left_operand)
+            if op == 'Print':
+                print("{0}{1}").format(quad.left_operand, quad.right_operand[2:-1])
             #basic operations +, - , *, /
             elif op == '+':
                 pass
@@ -54,16 +56,19 @@ class VirtualMachine(self):
                 pass
             # gotos
             elif op == 'Goto':
-                pc = quadruples[pc].res
+                self.pc = int(self.quadruples[self.pc].res)
                 continue
             elif op == 'GotoF':
                 dir = quad.left_operand
                 # if memory.getVal(dir) == 'false':
-                #     pc = quad.res
+                #     self.pc = quad.res
             elif op == 'GotoV':
                 dir = quad.left_operand
                 # if memory.getVal(dir) == 'true':
-                #     pc = quad.res
+                #     self.pc = quad.res
 
-            print quadruples[pc].printeame()
-            pc += 1
+            # print self.quadruples[self.pc].printeame()
+            self.pc += 1
+
+vm = VirtualMachine()
+vm.run()
