b6a1ba7fbf3f5b47e48d71a50181692c00a3492d
davilajose23 <davila.jose23@gmail.com>
2017-04-29 20:24:00 -0500

    added more tests factorial iterativo and fibo recursivo

--- graphics.py
+++ graphics.py
@@ -232,7 +232,8 @@ class GraphWin(tk.Canvas):
         master.lift()
         self.lastKey = ""
         if autoflush: _root.update()
-
+        scrollbar = tk.Scrollbar(master)
+        scrollbar.pack( side = tk.RIGHT, fill=tk.Y)
     def __repr__(self):
         if self.isClosed():
             return "<Closed GraphWin>"
